91e2928a17bcdc048bd6fd8b627b54174b6ca477
Kay Robbins <Kay.Robbins@utsa.edu>
2016-09-09 16:47:11 -0500

    Now compute blink statistics and save in blink file

--- blinker/moreExamples/showingStatistics/runExtractSummaryStatisticsOld.m
+++ blinker/moreExamples/showingStatistics/runExtractSummaryStatisticsOld.m
@@ -65,20 +65,34 @@ end
 % typeBlinks = 'EOGUnrefNewBothCombined';
 % excludeTasks = {'EO', 'EC'};
 
-%% Fill in an empty structure for efficiency
-blinkStatistics(numberFiles) = getSummaryStatistics();
+%% Read in the blink data for this collection
+
+%% Initialize
+blinkSummary(numberFiles) = struct( 'blinkFileName', nan, ...
+    'subjectID', nan, 'task', nan, 'uniqueName', nan, ...
+    'srate', nan, 'startTime', nan, ...
+    'usedNumber', nan, 'usedLabel', nan, 'status', nan, ...
+    'seconds', nan, 'numberBlinks', nan, 'numberGoodBlinks', nan, ... ...
+    'goodRatio', nan, 'header', nan, ...
+    'pAVRZ', nan, 'nAVRZ', nan, 'durationZ', nan, ...
+    'durationB', nan, 'durationT', nan, ...
+    'durationHZ', nan, 'durationHB', nan, 'blinksPerMin', nan);
+
+headers = {'mean', 'median', 'std', 'mad', 'goodMean', 'goodMedian', ...
+           'goodStd', 'goodMad'};
 for k = 1:numberFiles - 1
-    blinkStatistics(k) = getSummaryStatistics();
+    blinkSummary(k) = blinkSummary(numberFiles);
 end
 
 %% Now read in the individual files and process
+correlationThreshold = 0.98;
 mapGood = containers.Map('KeyType', 'char', 'ValueType', 'any');
 mapMarginal = containers.Map('KeyType', 'char', 'ValueType', 'any');
 fileMask = true(numberFiles, 1);
 nanMask = false(numberFiles, 1);
 
 for k = 1:numberFiles
-    clear blinks blinkFits blinkProperties blinkStatistics params;
+    clear blinks blinkFits blinkProperties params;
     thisFile = [blinkFiles(k).blinkFileName '_' typeBlinks '.mat'];
     if ~isKey(fileMap, thisFile)
         fileMask(k) = false;
@@ -102,18 +116,35 @@ for k = 1:numberFiles
         warning('---%s does not have blinks\n', fileName);
         continue;
     end
-    blinkStatistics(k) = ...
-                getBlinkStatistics(blinks, blinkFits, blinkProperties);
-    
-    if strcmpi(blinkStatistics(k).status, 'marginal')
+
+    %% Now the summary fields
+    sData = blinks.signalData;
+    signalNumbers = cellfun(@double, {sData.signalNumber});
+    pos = find(signalNumbers == abs(blinks.usedSignal), 1, 'first');
+    theLabel = lower(sData(pos).signalLabel);
+    blinkSummary(k).blinkFileName = thisFile;
+    blinkSummary(k).subjectID = blinks.subjectID;
+    blinkSummary(k).task = blinks.task;
+    blinkSummary(k).srate = blinks.srate;
+    blinkSummary(k).startTime = blinks.startTime;
+    blinkSummary(k).uniqueName = blinks.uniqueName;
+    blinkSummary(k).usedNumber = abs(blinks.usedSignal);
+    blinkSummary(k).usedLabel = theLabel;
+    blinkSummary(k).seconds = length(sData(pos).signal)/blinks.srate;
+    blinkSummary(k).numberBlinks = sData(pos).numberBlinks;
+    blinkSummary(k).numberGoodBlinks = sData(pos).numberGoodBlinks;
+    blinkSummary(k).goodRatio = sData(pos).goodRatio;
+    blinkSummary(k).header = headers;
+    if blinks.usedSignal < 0
         if isKey(mapMarginal, theLabel)
             theCount = mapMarginal(theLabel);
         else
             theCount = 0;
         end
         theCount = theCount + 1;
-        mapMarginal(theLabel) = theCount;    
-    elseif strcmpi(blinkStatistics(k).status, 'good')
+        mapMarginal(theLabel) = theCount;
+        blinkSummary(k).status = 'marginal';
+    else
         if isKey(mapGood, theLabel)
             theCount = mapGood(theLabel);
         else
@@ -121,8 +152,24 @@ for k = 1:numberFiles
         end
         theCount = theCount + 1;
         mapGood(theLabel) = theCount;
-        blinkStatistics(k).status = 'good';
+        blinkSummary(k).status = 'good';
     end
+    blinkSummary(k).pAVRZ = getOcularIndex(blinks, blinkProperties, ...
+        blinkFits, 'pAVRZ', correlationThreshold);
+    blinkSummary(k).nAVRZ = getOcularIndex(blinks, blinkProperties, ...
+        blinkFits, 'nAVRZ', correlationThreshold);
+    blinkSummary(k).durationZ = getOcularIndex(blinks, blinkProperties, ...
+        blinkFits, 'durationZ', correlationThreshold);
+    blinkSummary(k).durationB = getOcularIndex(blinks, blinkProperties, ...
+        blinkFits, 'durationB', correlationThreshold);
+    blinkSummary(k).durationT = getOcularIndex(blinks, blinkProperties, ...
+        blinkFits, 'durationT', correlationThreshold);
+    blinkSummary(k).durationHZ = getOcularIndex(blinks, blinkProperties, ...
+        blinkFits, 'durationHZ', correlationThreshold);
+    blinkSummary(k).durationHB = getOcularIndex(blinks, blinkProperties, ...
+        blinkFits, 'durationHB', correlationThreshold);
+    blinkSummary(k).blinksPerMin = getOcularIndex(blinks, blinkProperties, ...
+        blinkFits, 'blinksPerMin', correlationThreshold);
 end
 
 %% Now save the summary information
@@ -130,4 +177,4 @@ end
 
 %% Save the file
 save([summaryDir filesep summaryFile], ...
-    'blinkStatistics', 'mapGood', 'mapMarginal', 'nanMask', 'fileMask', '-v7.3');
+    'blinkSummary', 'mapGood', 'mapMarginal', 'nanMask', 'fileMask', '-v7.3');
